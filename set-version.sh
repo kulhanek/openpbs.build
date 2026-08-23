ec18bd93ea59fc60534b9e8c909524d6d732a3e3
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-08-23 17:26:50 +0200

    v4

--- set-version.sh
+++ set-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DEBFULLNAME="Petr Kulhanek" DEBEMAIL="kulhanek@chemi.muni.cz" dch -v 23.06.06-15+ub24lts-3 --distribution noble --urgency low --changelog openpbs.build/changelog
+DEBFULLNAME="Petr Kulhanek" DEBEMAIL="kulhanek@chemi.muni.cz" dch -v 23.06.06-15+ub24lts-4 --distribution noble --urgency low --changelog openpbs.build/changelog
